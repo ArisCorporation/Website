43264280eec5e63432afe1e24104a47a362c225b
lucas-gruber <blaumc@icloud.com>
2023-11-30 16:15:47 +0100

    fix: fixed member cards

--- components/global/membercards.vue
+++ components/global/membercards.vue
@@ -23,38 +23,38 @@ defineProps({
   >
     <template v-for="member in data.value" :key="member.id">
       <div
-        class="aspect-[270/320] text-center flex relative max-w-[270px] w-full mx-auto bg-image group"
-        :style="{
-          backgroundImage: `url(${$config.public.fileBase}${
-            member.potrait
-              ? member.potrait
-              : '0b7eafde-0933-4d1a-a32f-b4f8dd5bb492'
-          })`,
-        }"
+        class="aspect-[270/320] text-center relative max-w-[270px] w-full mx-auto bg-image group"
       >
-        <div
-          class="relative z-10 m-auto opacity-0 group-hover:opacity-100 transition-short-group"
-        >
-          <hr />
-          <p class="m-0">
-            Position: <span class="text-secondary">{{ member.position }}</span>
-          </p>
-          <template v-if="member.roles[0]">
+        <div class="absolute flex w-full h-full">
+          <div
+            class="z-20 m-auto opacity-0 group-hover:opacity-100 transition-short-group"
+          >
             <hr />
-            <p>“{{ member.roles?.join(', ') }}“</p>
-          </template>
-          <hr />
-          <p class="flex space-x-4 justify-center">
-            <NuxtLink :to="(ams ? '/ams/' : '/') + 'bigoraphy/' + member.slug"
-              >BIOGRAFIE</NuxtLink
-            >
-            <NuxtLink :to="'/ams/hangar/' + member.slug" v-if="hangarLink"
-              >HANGAR</NuxtLink
-            >
-          </p>
+            <p class="m-0">
+              Position:
+              <span class="text-secondary">{{ member.position }}</span>
+            </p>
+            <template v-if="member.roles[0]">
+              <hr />
+              <p>“{{ member.roles?.join(', ') }}“</p>
+            </template>
+            <hr />
+            <p class="flex space-x-4 justify-center">
+              <NuxtLink :to="(ams ? '/ams/' : '/') + 'bigoraphy/' + member.slug"
+                >BIOGRAFIE</NuxtLink
+              >
+              <NuxtLink :to="'/ams/hangar/' + member.slug" v-if="hangarLink"
+                >HANGAR</NuxtLink
+              >
+            </p>
+          </div>
         </div>
         <div
-          class="absolute w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-short-group"
+          class="absolute w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-short-group z-10"
+        />
+        <NuxtImg
+          :src="member.potrait || '0b7eafde-0933-4d1a-a32f-b4f8dd5bb492'"
+          class="w-full h-full z-0"
         />
       </div>
     </template>
