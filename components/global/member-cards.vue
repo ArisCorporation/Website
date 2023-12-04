a62a381e58ae76d04d62bd591bd8e912408d1c50
lucas-gruber <blaumc@icloud.com>
2023-12-04 19:59:18 +0100

    feat: member cards

--- components/global/member-cards.vue
+++ components/global/member-cards.vue
@@ -14,21 +14,15 @@ defineProps({
     type: Object as PropType<Member>,
     required: false,
   },
-})
+});
 </script>
 
 <template>
-  <div
-    class="grid lg:grid-cols-4 grid-cols-1 md:grid-cols-2 gap-y-12 gap-x-6 justify-center"
-  >
+  <div class="grid justify-center grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-y-12 gap-x-6">
     <template v-for="member in data.value" :key="member.id">
-      <div
-        class="aspect-[270/320] text-center relative max-w-[270px] w-full mx-auto bg-image group"
-      >
+      <div class="aspect-[270/320] text-center relative max-w-[270px] w-full mx-auto bg-image group">
         <div class="absolute flex w-full h-full">
-          <div
-            class="z-20 m-auto opacity-0 group-hover:opacity-100 transition-short-group"
-          >
+          <div class="z-20 m-auto opacity-0 group-hover:opacity-100 transition-short-group">
             <hr />
             <p class="m-0">
               Position:
@@ -39,23 +33,14 @@ defineProps({
               <p>“{{ member.roles?.join(', ') }}“</p>
             </template>
             <hr />
-            <p class="flex space-x-4 justify-center">
-              <NuxtLink :to="(ams ? '/ams/' : '/') + 'bigoraphy/' + member.slug"
-                >BIOGRAFIE</NuxtLink
-              >
-              <NuxtLink :to="'/ams/hangar/' + member.slug" v-if="hangarLink"
-                >HANGAR</NuxtLink
-              >
+            <p class="flex justify-center space-x-4">
+              <NuxtLink :to="(ams ? '/ams/' : '/') + 'bigoraphy/' + member.slug">BIOGRAFIE</NuxtLink>
+              <NuxtLink v-if="hangarLink" :to="'/ams/hangar/' + member.slug">HANGAR</NuxtLink>
             </p>
           </div>
         </div>
-        <div
-          class="absolute w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-short-group z-10"
-        />
-        <NuxtImg
-          :src="member.potrait || '0b7eafde-0933-4d1a-a32f-b4f8dd5bb492'"
-          class="w-full h-full z-0"
-        />
+        <div class="absolute z-10 w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-short-group" />
+        <NuxtImg :src="member.potrait || '0b7eafde-0933-4d1a-a32f-b4f8dd5bb492'" class="z-0 w-full h-full" />
       </div>
     </template>
   </div>
