56943cd4fd909a476479d29196cacd26b853fe87
lucas-gruber <blaumc@icloud.com>
2023-12-12 18:28:28 +0100

    feat: added custom error page

---
+++ error.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { error } = defineProps({
+  error: {
+    type: Object,
+    default: null,
+  },
+});
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
+
+<template>
+  <div>
+    <div class="pt-16 text-center">
+      <h1 class="uppercase text-primary">Status Code: {{ error.statusCode }}</h1>
+      <h2 class="uppercase text-secondary">{{ error.statusMessage }}</h2>
+    </div>
+    <div class="pt-12 mx-auto text-center">
+      <button
+        class="p-2 transition rounded-full text-bsecondary hover:cursor-pointer hover:bg-secondary/50 hover:no-underline bg-secondary"
+        @click="handleError"
+      >
+        Take me Back on Course!
+      </button>
+    </div>
+  </div>
+</template>
