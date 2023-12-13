ec6df48a9a77dd278d7d4f8fccff5be25a06b4e1
lucas-gruber <blaumc@icloud.com>
2023-12-13 19:07:39 +0100

    feat: added default button

---
+++ components/global/button/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <button
+    class="relative inline-block w-full transition border-2 cursor-pointer p-2px group rounded-[10px] after:-bottom-2px after:absolute after:bg-bsecondary after:h-2px after:left-4 after:right-4 before:-top-2px before:absolute before:bg-bsecondary before:box-border before:h-2px before:left-4 before:right-4 bg-bprimary border-primary text-inherit"
+  >
+    <div
+      class="box-border p-2 overflow-hidden text-center rounded-md transition-group group-hover:bg-btertiary group-hover:text-primary whitespace-nowrap text-ellipsis"
+    >
+      <p class="p-0"><slot /></p>
+    </div>
+  </button>
+</template>
