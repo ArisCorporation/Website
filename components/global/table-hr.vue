0e90f4516fa1ac7babddafb3d16fe2fe99b1b3b7
lucas-gruber <blaumc@icloud.com>
2023-12-13 23:41:08 +0100

    fix: fixed table hr

--- components/global/table-hr.vue
+++ components/global/table-hr.vue
@@ -1,3 +1,9 @@
 <template>
-  <hr class="hr-table" />
+  <hr class="text-primary" />
 </template>
+
+<style scoped lang="postcss">
+hr {
+  @apply relative mt-3 mb-2 -ml-1 col-span-full sm:mt-3 sm:mb-2 bg-bsecondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary;
+}
+</style>
