669005b6276c16ea9312c85862fcb9a7b4805b96
lucas-gruber <blaumc@icloud.com>
2023-12-14 01:37:07 +0100

    fix: fixed member transform feat: added tabid to departments

--- utils/transform-department.ts
+++ utils/transform-department.ts
@@ -2,6 +2,24 @@ export default function (obj: any) {
   const getShips = () => obj.ships?.map((i: any) => transformHangarItem(i));
   const getMembers = () => obj.members?.map((i: any) => transformMember(i));
   const getHoD = () => (obj.head_of_department ? transformMember(obj.head_of_department) : null);
+  const getTabId = () => {
+    if (obj.gameplay_name) {
+      if (obj.gameplay_name === 'Bergbau') return 0;
+      if (obj.gameplay_name === 'Bergung') return 1;
+      if (obj.gameplay_name === 'Betankung') return 2;
+      if (obj.gameplay_name === 'Datentransport') return 3;
+      if (obj.gameplay_name === 'Erkundung') return 4;
+      if (obj.gameplay_name === 'Konstruktion') return 5;
+      if (obj.gameplay_name === 'Logistik') return 6;
+      if (obj.gameplay_name === 'Medizin') return 7;
+      if (obj.gameplay_name === 'Personentransport') return 8;
+      if (obj.gameplay_name === 'Reparatur und Wartung') return 9;
+      if (obj.gameplay_name === 'Sicherheit') return 10;
+      if (obj.gameplay_name === 'Wissenschaft') return 11;
+    } else {
+      return null;
+    }
+  };
 
   return {
     id: obj.id,
@@ -13,5 +31,6 @@ export default function (obj: any) {
     ships: getShips(),
     employees: getMembers(),
     head_of_department: getHoD(),
+    tabId: getTabId(),
   };
 }
