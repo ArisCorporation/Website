4558866a1739d17ed70f35d638f11968e4cdd68e
lucas-gruber <blaumc@icloud.com>
2023-12-15 15:41:06 +0100

    fix: fixed ve index

--- pages/VerseExkurs/home/index.vue
+++ pages/VerseExkurs/home/index.vue
@@ -24,8 +24,8 @@ definePageMeta({
 
 <template>
   <div>
-    <div class="prose prose-invert" v-html="data" />
-    <div class="flex flex-wrap justify-between px-4 mx-auto mt-12 mb-8">
+    <div class="max-w-full prose prose-invert" v-html="data" />
+    <div class="flex flex-wrap justify-between gap-4 px-4 mx-auto mt-12 mb-8">
       <NuxtLink to="https://robertsspaceindustries.com/starmap" target="_blank">
         <IconHover name="IconsLogosArk" size="48" />
       </NuxtLink>
