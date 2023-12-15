89a35004990d7f315565a29af0aa6a2eaca6890f
lucas-gruber <blaumc@icloud.com>
2023-12-15 01:30:34 +0100

    ref: refactored layout levels

--- layouts/verse-exkurs.vue
+++ layouts/verse-exkurs.vue
@@ -1,3 +1,11 @@
 <template>
-  <slot />
+  <div>
+    <VerseExkursSidebar />
+    <div class="flex flex-col justify-between flex-1 min-h-screen lg:ml-64">
+      <div class="container px-4 mx-auto">
+        <slot />
+      </div>
+      <Footer />
+    </div>
+  </div>
 </template>
