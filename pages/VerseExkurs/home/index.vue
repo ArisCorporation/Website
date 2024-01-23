621355e781e10616ea7b5729a31a865684a22e53
lucas-gruber <blaumc@icloud.com>
2024-01-23 21:13:37 +0100

    Remove unused CSS classes and div element

--- pages/VerseExkurs/home/index.vue
+++ pages/VerseExkurs/home/index.vue
@@ -14,8 +14,6 @@ const { data } = await useAsyncData(
   },
 );
 
-// h-48 w-48
-
 definePageMeta({
   path: '/VerseExkurs',
   layout: 'verse-exkurs',
@@ -25,7 +23,6 @@ definePageMeta({
 <template>
   <div>
     <div class="prose prose-invert" v-html="data" />
-    <div class="w-96 h-96"></div>
     <div class="flex flex-wrap justify-between gap-4 px-4 mx-auto mt-12 mb-8">
       <NuxtLink to="https://robertsspaceindustries.com/starmap" target="_blank">
         <IconHover name="IconsLogosArk" size="48" />
