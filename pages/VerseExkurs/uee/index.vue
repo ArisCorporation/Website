1c59694551eb00fcd37d6dce4fccd1b8bdd34924
lucas-gruber <blaumc@icloud.com>
2024-02-15 15:24:26 +0100

    feat: added ve-uee section

--- pages/VerseExkurs/uee/index.vue
+++ pages/VerseExkurs/uee/index.vue
@@ -1,69 +1,65 @@
 <script setup lang="ts">
-const { getSingletonItem } = useDirectusItems();
+const { readSingleton } = useDirectusItems();
 const VeTabs = useVeTabsStore();
 
-const { data } = await useAsyncData(
-  'getUEE',
-  () =>
-    getSingletonItem({
-      collection: 'united_empire_of_earth',
-      params: {
-        fields: ['title', 'image.id', 'text', 'sections', 'feiertage'],
-      },
-    }),
-  {
-    transform: (data) => ({
-      title: data.title,
-      banner: data.image?.id,
-      content: data.text,
-      tablist: data.sections.map((obj) => ({
-        header: obj.title,
-        content: obj.content,
-      })),
-      holidayTablist: data.feiertage.map((obj) => ({
-        header: obj.name,
-        date: obj.datum,
-        content: obj.beschreibung,
-      })),
-      sections: data.sections,
-      holidays: data.feiertage,
-    }),
-  },
-);
+const dataRes = await readSingleton('uee', {
+  fields: ['banner', 'content', 'tabs', 'holidays'],
+});
+const data = computed(() => ({
+  banner: dataRes.banner,
+  content: dataRes.content,
+  tablist: dataRes.tabs.map((obj) => ({
+    header: obj.title,
+    content: obj.content,
+  })),
+  holidayTablist: dataRes.holidays.map((obj) => ({
+    header: obj.name,
+    date: obj.date,
+    content: obj.content,
+  })),
+  sections: dataRes.tabs,
+  holidays: data.holidays,
+}));
 
+if (!data) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Die Übertragung konnte nicht vollständig empfangen werden!',
+    fatal: true,
+  });
+}
+
+useHead({
+  title: 'UEE',
+});
 definePageMeta({
   layout: 'verse-exkurs',
 });
 </script>
 
 <template>
-  <div>
-    <div>
-      <h1>{{ data?.title }}</h1>
-      <hr />
-      <NuxtImg :src="data?.banner" class="mx-auto" />
-    </div>
-    <div>
-      <h2>VersExkurs: United Empire of Earth</h2>
-      <hr class="hr-short" />
-      <div v-html="data?.content" />
-      <TabGroup :tablist="data?.tablist" small-header :store="VeTabs.selectedUeeTab" :change="VeTabs.setUeeTab">
-        <template #tabcontent>
-          <div>
-            <div
-              v-if="data?.tablist[VeTabs.selectedUeeTab].header !== 'Feiertage & Events'"
-              v-html="data?.tablist[VeTabs.selectedUeeTab]?.content"
+  <VeBaseArticle>
+    <template #title>
+      Alles über das <span class="text-aris-400">United Empire of Earth</span>
+      <NuxtImg :src="data?.banner" :placeholder="[16, 16, 1, 5]" class="mx-auto" />
+    </template>
+    <div v-html="data?.content" />
+    <TabGroup :tablist="data?.tablist" small-header :store="VeTabs.selectedUeeTab" :change="VeTabs.setUeeTab">
+      <template #tabcontent>
+        <div>
+          <div
+            v-if="data?.tablist[VeTabs.selectedUeeTab].header !== 'Feiertage & Events'"
+            v-html="data?.tablist[VeTabs.selectedUeeTab]?.content"
+          />
+          <div v-else>
+            <TabGroupVertical
+              :tablist="data?.holidayTablist"
+              :store="VeTabs.selectedUeeHolidayTab"
+              :change="VeTabs.setUeeHolidayTab"
             />
-            <div v-else>
-              <TabGroupVertical
-                :tablist="data?.holidayTablist"
-                :store="VeTabs.selectedUeeHolidayTab"
-                :change="VeTabs.setUeeHolidayTab"
-              />
-            </div>
           </div>
-        </template>
-      </TabGroup>
-    </div>
-  </div>
+        </div>
+      </template>
+    </TabGroup>
+  </VeBaseArticle>
 </template>
