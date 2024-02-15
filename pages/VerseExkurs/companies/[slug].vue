9d64fff375c9d62ec4e0ef99de5c219ca76cdd82
lucas-gruber <blaumc@icloud.com>
2024-02-15 15:24:54 +0100

    feat: added companies and fractions to ve

---
+++ pages/VerseExkurs/companies/[slug].vue
@@ -0,0 +1,212 @@
+<script setup lang="ts">
+import type { Data } from 'unified/lib';
+
+const route = useRoute();
+const { readItems } = useDirectusItems();
+const selectedTab = ref(0);
+
+const dataRes = await readItems('companies', {
+  fields: [
+    'name',
+    'banner',
+    'content',
+    'category.id',
+    'category.name',
+    'current_ceo',
+    'founded',
+    'founder',
+    'famous_goods',
+    'headquarter.collection',
+    'headquarter.headquarter.name',
+    'headquarter.headquarter.slug',
+    'headquarter.headquarter.planet.name',
+    'headquarter.headquarter.planet.slug',
+    'headquarter.headquarter.moon.name',
+    'headquarter.headquarter.moon.slug',
+  ],
+  filter: {
+    slug: { _eq: route.params.slug },
+  },
+  limit: 1,
+});
+
+// const test = await readItems('systems', {
+//   fields: ['name', 'orbit.object:planets.name'],
+//   filter: {
+//     orbit: {
+//       'object:planets': {
+//         name: { _eq: 'Crusader' },
+//       },
+//     },
+//   },
+//   limit: -1,
+// });
+
+if (!dataRes[0]) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Die Übertragung konnte nicht vollständig empfangen werden!',
+    fatal: true,
+  });
+}
+
+const data = computed(() => transformCompany(dataRes[0]));
+
+if (
+  data.value.headquarter?.collection === 'planets' ||
+  data.value.headquarter?.collection === 'moons' ||
+  data.value.headquarter?.collection === 'landing_zones'
+) {
+  const system = await readItems('systems', {
+    fields: ['name', 'orbit.object:planets.name'],
+    filter: {
+      orbit: data.value.headquarter.planet
+        ? {
+            'object:planets': {
+              name: { _eq: data.value.headquarter.planet.name },
+            },
+          }
+        : {
+            'object:moons': {
+              name: { _eq: data.value.headquarter.moon.name },
+            },
+          },
+    },
+    limit: 1,
+  });
+  if (system[0]) {
+    data.value.headquarter.system = { name: system[0].name, slug: system[0].slug };
+  }
+}
+
+useHead({
+  title: data.value.name,
+});
+definePageMeta({
+  layout: 'verse-exkurs',
+});
+</script>
+
+<template>
+  <VeBaseArticle :banner="data?.banner">
+    <template #title>
+      Firma:
+      <span class="text-primary"> {{ data?.name }}</span>
+    </template>
+    <div>
+      <!-- <DefaultPanel class="w-full mb-8 ml-12 xl:float-right xl:w-1/2"> -->
+      <TableParent title="Infobox" class="w-full mb-8 xl:ml-12 xl:float-right xl:w-1/2">
+        <!-- Stanton / ArcCorp / Area18 -->
+        <TableRow
+          v-if="data.headquarter"
+          title="Hauptsitz"
+          :content="
+            data.headquarter?.collection === 'systems'
+              ? data.headquarter.name
+              : data.headquarter?.collection === 'planets'
+                ? data.headquarter.name
+                : data.headquarter?.collection === 'landing_zones' && data.headquarter.name
+          "
+          full-width
+        >
+          <span class="my-auto">
+            <NuxtLink
+              v-if="data.headquarter.system"
+              :to="'/VerseExkurs/starmap/' + data.headquarter.system.slug"
+              class="my-auto transition opacity-75 decoration-transparent hover:opacity-100 text-aris-400"
+            >
+              {{ data.headquarter.system.name }}
+            </NuxtLink>
+            <template v-if="data.headquarter.system && data.headquarter.planet"> / </template>
+            <NuxtLink
+              v-if="data.headquarter.planet"
+              :to="'/VerseExkurs/starmap/' + data.headquarter.system?.slug + '?planet=' + data.headquarter.planet.slug"
+              class="my-auto transition opacity-75 decoration-transparent hover:opacity-100 text-aris-400"
+            >
+              {{ data.headquarter.planet.name }}
+            </NuxtLink>
+            <template v-if="data.headquarter.planet && data.headquarter.name"> / </template>
+            <NuxtLink
+              v-if="data.headquarter.name"
+              :to="
+                '/VerseExkurs/starmap/' +
+                data.headquarter.system.slug +
+                '?planet=' +
+                data.headquarter.planet.slug +
+                '?landing_zones=' +
+                data.headquarter.slug
+              "
+              class="my-auto transition opacity-75 decoration-transparent hover:opacity-100 text-aris-400"
+            >
+              {{ data.headquarter.name }}
+            </NuxtLink>
+          </span>
+        </TableRow>
+        <TableRow title="Gründungsdatum" :content="data?.founded" full-width />
+        <TableRow title="Gründer" :content="data?.founder" full-width />
+        <TableRow title="Kategorie" :content="data?.category.name" full-width />
+        <TableRow title="Bekannteste Waren" :content="data?.famous_goods" full-width />
+      </TableParent>
+      <!-- </DefaultPanel> -->
+      <div v-html="data?.content" />
+      <hr />
+      <h3>Waren der Firma {{ data?.name }}</h3>
+      <!-- <Disclosure v-if="data.ships[0]">
+        <template #title>
+          Schiffe von <span class="text-primary-400">{{ data.name }}</span>
+        </template>
+        <div class="flex flex-wrap">
+          <ShipCard
+            v-for="ship in data?.ships"
+            :key="ship.id"
+            :ship-data="ship.ship"
+            display-department
+            preload-images
+          />
+        </div>
+      </Disclosure>
+      <Disclosure v-if="data.modules[0]">
+        <template #title>
+          Schiffsmodule von <span class="text-primary-400">{{ data.name }}</span>
+        </template>
+        <div class="flex flex-wrap">
+          <ShipCard
+            v-for="ship in data?.modules"
+            :key="ship.id"
+            :ship-data="ship.ship"
+            display-department
+            preload-images
+          />
+        </div>
+      </Disclosure>
+      <Disclosure v-if="data.weapons[0]">
+        <template #title>
+          Waffen von <span class="text-primary-400">{{ data.name }}</span>
+        </template>
+        <div class="flex flex-wrap">
+          <ShipCard
+            v-for="ship in data?.weapons"
+            :key="ship.id"
+            :ship-data="ship.ship"
+            display-department
+            preload-images
+          />
+        </div>
+      </Disclosure>
+      <Disclosure v-if="data.weapon_attachments[0]">
+        <template #title>
+          Waffenaufsätze von <span class="text-primary-400">{{ data.name }}</span>
+        </template>
+        <div class="flex flex-wrap">
+          <ShipCard
+            v-for="ship in data?.weapon_attachments"
+            :key="ship.id"
+            :ship-data="ship.ship"
+            display-department
+            preload-images
+          />
+        </div>
+      </Disclosure> -->
+    </div>
+  </VeBaseArticle>
+</template>
---
+++ pages/VerseExkurs/companies/index.vue
@@ -0,0 +1,139 @@
+<script setup lang="ts">
+const { readItems } = useDirectusItems();
+const selectedMainTab = ref(0);
+const selectedSubTab = ref(0);
+
+const categories = await readItems('company_categories', {
+  fields: ['name', 'sub_categories.name', 'sub_categories.sub_categories.name'],
+  filter: { superior_category: { _null: true } },
+  sort: ['sort'],
+});
+
+const companies = await readItems('companies', {
+  fields: ['name', 'slug', 'logo', 'category.name'],
+  filter: { status: { _eq: 'published' } },
+  sort: ['name'],
+  limit: -1,
+});
+
+if (!categories || !companies) {
+  throw createError({
+    statusCode: 500,
+    statusMessage: 'Die Übertragung konnte nicht vollständig empfangen werden!',
+    fatal: true,
+  });
+}
+
+const mainCategory = computed(() => categories[selectedMainTab.value]);
+const subCategories = computed(() => categories[selectedMainTab.value].sub_categories);
+const subSubCategories = computed(
+  () => categories[selectedMainTab.value].sub_categories[selectedSubTab.value].sub_categories,
+);
+
+const subTabs = computed(() =>
+  categories[selectedMainTab.value].sub_categories.map((obj) => ({
+    header: obj.name,
+  })),
+);
+
+const changeMainTab = async (index: number) => {
+  selectedSubTab.value = 0;
+  selectedMainTab.value = index;
+};
+
+useHead({
+  title: 'Firmen',
+});
+definePageMeta({
+  layout: 'verse-exkurs',
+});
+</script>
+
+<template>
+  <div>
+    <TabGroup
+      :tablist="
+        categories.map((obj) => ({
+          header: obj.name,
+        }))
+      "
+      between
+      :store="selectedMainTab"
+      :change="changeMainTab"
+    >
+      <template #tabcontent>
+        <TabGroup
+          v-if="subCategories[0]"
+          :tablist="subTabs"
+          hide-hr
+          small-header
+          between
+          no-margin
+          :store="selectedSubTab"
+          :change="(index: number) => (selectedSubTab = index)"
+        >
+          <template #tabcontent>
+            <div v-if="subSubCategories[0]" v-for="category in subSubCategories" :key="category.name">
+              <TableHr
+                v-if="
+                  companies.filter(
+                    (obj: any) => obj.category.name === subSubCategories.find((obj) => obj.name === category.name).name,
+                  )[0]
+                "
+              >
+                {{ category.name }}
+              </TableHr>
+              <div class="company-grid">
+                <NuxtLink
+                  v-for="company in companies
+                    .filter(
+                      (obj: any) =>
+                        obj.category.name === subSubCategories.find((obj) => obj.name === category.name).name,
+                    )
+                    .sort((a, b) => a.name.localeCompare(b.name))"
+                  :key="company.slug"
+                  :to="'/VerseExkurs/companies/' + company.slug"
+                  class="w-full h-full transition opacity-50 hover:opacity-100"
+                >
+                  <NuxtImg :src="company.logo" :placeholder="[16, 16, 1, 5]" class="w-full h-full" />
+                </NuxtLink>
+              </div>
+            </div>
+            <div v-else class="company-grid">
+              <NuxtLink
+                v-for="company in companies
+                  .filter((obj: any) => obj.category.name === subCategories[selectedSubTab].name)
+                  .sort((a, b) => a.name.localeCompare(b.name))"
+                :key="company.slug"
+                :to="'/VerseExkurs/companies/' + company.slug"
+                class="w-full h-full transition opacity-50 hover:opacity-100"
+              >
+                <NuxtImg :src="company.logo" :placeholder="[16, 16, 1, 5]" class="w-full h-full" />
+              </NuxtLink>
+            </div>
+          </template>
+        </TabGroup>
+        <template v-else>
+          <div class="company-grid">
+            <NuxtLink
+              v-for="company in companies
+                .filter((obj: any) => obj.category.name === mainCategory.name)
+                .sort((a, b) => a.name.localeCompare(b.name))"
+              :key="company.slug"
+              :to="'/VerseExkurs/companies/' + company.slug"
+              class="w-full h-full transition opacity-50 hover:opacity-100"
+            >
+              <NuxtImg :src="company.logo" :placeholder="[16, 16, 1, 5]" class="w-full h-full" />
+            </NuxtLink>
+          </div>
+        </template>
+      </template>
+    </TabGroup>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.company-grid {
+  @apply grid w-full grid-cols-4 gap-8;
+}
+</style>
