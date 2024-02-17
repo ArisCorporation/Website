45445cc155a8e26b9a491ff44ecd07458f66ac6c
lucas-gruber <blaumc@icloud.com>
2024-02-17 14:57:38 +0100

    fix: fixed table hr

--- components/global/table-hr.vue
+++ components/global/table-hr.vue
@@ -7,7 +7,7 @@
 
 <style scoped lang="postcss">
 hr {
-  @apply m-0 my-auto relative bg-bsecondary text-primary-400 before:w-1 before:aspect-[1/1] before:absolute before:inline-block before:bg-primary-400 after:w-1 after:right-0 after:aspect-[1/1] after:absolute after:inline-block after:bg-primary-400;
+  @apply m-0 my-auto relative bg-bsecondary text-primary-400 before:left-0 before:w-1 before:aspect-[1/1] before:absolute before:inline-block before:bg-primary-400 after:w-1 after:right-0 after:aspect-[1/1] after:absolute after:inline-block after:bg-primary-400;
 }
 div {
   @apply flex -mx-1 mt-3 mb-3 col-span-full;
