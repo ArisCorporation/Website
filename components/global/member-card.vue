b9ed9edad82c1da337ea261f05ba02ccdc71ebd2
lucas-gruber <blaumc@icloud.com>
2024-04-06 18:37:38 +0200

    fix: fixed member and ship-cards

--- components/global/member-card.vue
+++ components/global/member-card.vue
@@ -11,7 +11,7 @@ defineProps({
     default: false,
   },
   data: {
-    type: Object as PropType<IMember>,
+    type: Object as PropType<IUser>,
     required: true,
   },
   hidden: {
@@ -23,7 +23,7 @@ defineProps({
 </script>
 
 <template>
-  <Presence exit-before-enter>
+  <!-- <Presence exit-before-enter>
     <Motion
       v-if="!hidden"
       :key="data.id"
@@ -32,38 +32,44 @@ defineProps({
       :exit="{ opacity: 0, y: -15 }"
       :transition="{ duration: 0.5 }"
       class="relative w-full px-4 mb-4 text-center basis-1/2 lg:basis-1/3 xl:basis-1/4"
-    >
-      <div class="aspect-potrait mx-auto relative max-w-[270px] w-full group rounded-sm border-secondary border-b-2">
-        <div class="absolute flex w-full h-full text-xs sm:text-base">
-          <div class="z-20 m-auto opacity-0 group-hover:opacity-100 transition-short-group">
+    > -->
+  <div v-if="!hidden" :key="data.id" class="relative w-full px-4 mb-4 text-center basis-1/2 lg:basis-1/3 xl:basis-1/4">
+    <div class="aspect-potrait mx-auto relative max-w-[270px] w-full group rounded-sm border-secondary border-b-2">
+      <div class="absolute flex w-full h-full text-xs sm:text-base">
+        <div class="z-20 m-auto opacity-0 group-hover:opacity-100 transition-short-group">
+          <hr />
+          <p>
+            Position:
+            <span class="text-secondary">{{ data.position?.name }}</span>
+          </p>
+          <template v-if="data.roles">
             <hr />
-            <p>
-              Position:
-              <span class="text-secondary">{{ data.position?.name }}</span>
-            </p>
-            <template v-if="data.roles">
-              <hr />
-              <p>&ldquo;{{ data.roles?.join(', ') }}&ldquo;</p>
-            </template>
-            <hr />
-            <p class="flex justify-center space-x-4">
-              <NuxtLink :to="ams ? data.biography_ams_link : data.biography_link">BIOGRAFIE</NuxtLink>
-              <NuxtLink v-if="hangarLink" :to="data.hangar_link">HANGAR</NuxtLink>
-            </p>
-          </div>
+            <p>&ldquo;{{ data.roles?.join(', ') }}&ldquo;</p>
+          </template>
+          <hr />
+          <p class="flex justify-center space-x-4">
+            <NuxtLink :to="ams ? data.biography_ams_link : data.biography_link" class="animate-link hover:brightness-85"
+              >BIOGRAFIE</NuxtLink
+            >
+            <NuxtLink v-if="hangarLink" :to="data.hangar_link" class="animate-link hover:brightness-85"
+              >HANGAR</NuxtLink
+            >
+          </p>
         </div>
-        <div class="absolute z-10 w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-short-group" />
-        <NuxtImg
-          :src="data.avatar"
-          :placeholder="[16, 16, 1, 5]"
-          :alt="'Potrait von ' + data.full_name"
-          class="z-0 object-cover w-full h-full"
-        />
       </div>
-      <h4>{{ data.full_name }}</h4>
-      <h6 class="pt-0 text-light-gray">{{ data.position?.name }}</h6>
-    </Motion>
-  </Presence>
+      <div class="absolute z-10 w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-short-group" />
+      <NuxtImg
+        :src="data.avatar"
+        :placeholder="[16, 16, 1, 5]"
+        :alt="'Potrait von ' + data.full_name"
+        class="z-0 object-cover w-full h-full"
+      />
+    </div>
+    <h4>{{ data.full_name }}</h4>
+    <h6 class="pt-0 text-light-gray">{{ data.position?.name }}</h6>
+  </div>
+  <!-- </Motion> -->
+  <!-- </Presence> -->
 </template>
 
 <style scoped lang="postcss">
--- components/global/ship-card.vue
+++ components/global/ship-card.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-const modalStore = useModalStore();
 const removePopover = ref(false);
 const emit = defineEmits(['edit', 'removeOpen', 'removeConfirm']);
 const props = defineProps({
@@ -13,6 +12,11 @@ const props = defineProps({
     default: null,
   },
   displayOwner: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  displayPlannedState: {
     type: Boolean,
     required: false,
     default: false,
@@ -90,162 +94,162 @@ const handleEdit = () => {
 // border-danger text-danger border-success text-success
 </script>
 <template>
-  <Presence exit-before-enter>
-    <Motion
-      v-if="!hidden"
-      :key="hangarData.id ? hangarData.id : shipData.id"
-      :initial="{ opacity: 0, y: -15 }"
-      :animate="{ opacity: 1, y: 0 }"
-      :exit="{ opacity: 0, y: -15 }"
-      :transition="{ duration: 0.5 }"
-      class="static block px-2 pb-3 basis-full md:basis-1/2 xl:basis-1/3 3xl:basis-1/4"
-    >
-      <DefaultPanel :color="color">
-        <div class="relative h-fit">
-          <NuxtLink
-            :to="'/ShipExkurs/' + shipData.slug"
-            class="block relative transition-all duration-500 ease h-[200px] bg-image group peer"
-          >
-            <NuxtImg :preload="preloadImages" class="absolute object-cover w-full h-full" :src="shipData.storeImage" />
-            <div
-              v-if="displayProductionState || displayLoanerState"
-              class="absolute top-1 left-2 text-stroke"
-              :class="{
-                'text-primary-400': shipData.productionState == 'Flugfertig',
-                'text-secondary': shipData.productionState == 'In Produktion',
-                'text-white': shipData.productionState == 'Im Konzept',
-              }"
-            >
-              <p v-if="displayProductionState" class="block p-0">{{ shipData.productionState }}</p>
-              <p v-if="displayLoanerState && hangarData.loaner" class="block p-0 text-secondary">Loaner</p>
-              <p
-                v-if="displayHiddenState && hangarData.userData.visibility === 'hidden'"
-                class="block p-0 text-secondary"
-              >
-                Versteckt
-              </p>
-            </div>
-            <div
-              v-if="displayDepartment && hangarData.userData.department"
-              class="absolute z-10 flex flex-row-reverse h-16 right-2 top-1"
-            >
-              <NuxtImg
-                :preload="preloadImages"
-                class="relative w-16 h-16 peer/departmentLogo text-secondary"
-                :src="hangarData.userData.department.logo"
-              />
-              <span
-                class="px-1 my-auto mr-2 transition rounded opacity-0 cursor-pointer h-fit bg-bsecondary peer-hover/departmentLogo:opacity-100"
-                >{{ hangarData.userData.department.name }}</span
-              >
-            </div>
-          </NuxtLink>
+  <div class="static block px-2 pb-3 basis-full md:basis-1/2 xl:basis-1/3 3xl:basis-1/4">
+    <DefaultPanel :color="color">
+      <div class="relative h-fit">
+        <NuxtLink
+          :to="'/shipexkurs/ships/' + shipData.slug"
+          class="block relative transition-all duration-500 ease h-[200px] bg-image group peer"
+        >
+          <NuxtImg
+            :src="shipData.store_image"
+            :placeholder="[16, 16, 1, 5]"
+            :preload="preloadImages"
+            height="500"
+            class="absolute object-cover w-full h-full"
+          />
           <div
-            class="peer-hover:[&>a:nth-child(1)]:opacity-100 peer-hover:[&>p:nth-child(1)]:duration-300 absolute z-10 bottom-0 left-0 flex flex-wrap w-full min-h-[48px] px-4 py-1 bg-bsecondary/80 justify-between gap-x-4"
+            v-if="displayProductionState || displayLoanerState"
+            class="absolute top-1 left-2 text-stroke"
+            :class="{
+              'text-primary-400': shipData.production_status_value == 'flight-ready',
+              'text-secondary': shipData.production_status_value == 'in-production',
+              'text-white': shipData.production_status_value == 'in-concept',
+            }"
           >
-            <div
-              v-if="displayOwner && hangarData.userData.planned"
-              class="absolute -top-6 right-2 text-stroke text-secondary"
+            <p v-if="displayProductionState" class="block p-0">{{ shipData.production_status }}</p>
+            <p v-if="displayLoanerState && hangarData.loaner" class="block p-0 text-secondary">Loaner</p>
+            <p
+              v-if="displayHiddenState && hangarData.userData.visibility === 'hidden'"
+              class="block p-0 text-secondary"
             >
-              Geplant
-            </div>
-            <NuxtLink
-              :to="'/ShipExkurs/' + shipData.slug"
-              class="m-0 transition hover:no-underline basis-full opacity-80 text-secondary hover:opacity-100"
+              Versteckt
+            </p>
+          </div>
+          <div
+            v-if="displayDepartment && hangarData.userData.department && hangarData.userData.department.logo"
+            class="absolute z-10 flex flex-row-reverse h-16 right-2 top-1"
+          >
+            <NuxtImg
+              :src="hangarData.userData.department.logo"
+              :placeholder="[16, 16, 1, 5]"
+              :preload="preloadImages"
+              class="relative w-16 h-16 peer/departmentLogo text-secondary"
+            />
+            <span
+              class="px-1 my-auto mr-2 transition rounded opacity-0 cursor-pointer h-fit bg-bsecondary peer-hover/departmentLogo:opacity-100"
+              >{{ hangarData.userData.department.name }}</span
             >
-              {{ shipData.name }}
-              <span v-if="displayName && hangarData.userData.name"> - &quot;{{ hangarData.userData.name }}&quot;</span>
-            </NuxtLink>
-            <NuxtLink
-              :to="'/VerseExkurs/companies/' + shipData.manufacturer.slug"
-              class="z-20 min-w-0 min-h-0 mt-auto text-xs text-white opacity-50 w-fit h-fit transition-group hover:no-underline hover:opacity-100"
-              >{{ shipData.manufacturer.name }}
-            </NuxtLink>
-            <NuxtLink
-              v-if="displayOwner"
-              :to="(internalBio ? '/ams/' : '/') + 'biography/' + hangarData.userData.owner.slug"
-              class="z-20 block mt-auto ml-auto text-xs text-white transition opacity-50 hover:no-underline hover:opacity-100"
+          </div>
+        </NuxtLink>
+        <div
+          class="peer-hover:[&>a:nth-child(1)]:opacity-100 peer-hover:[&>p:nth-child(1)]:duration-300 absolute z-10 bottom-0 left-0 flex flex-wrap w-full min-h-[48px] px-4 py-1 bg-bsecondary/80 justify-between gap-x-4"
+        >
+          <div
+            v-if="displayPlannedState && hangarData.userData.planned"
+            class="absolute -top-6 right-2 text-stroke text-secondary"
+          >
+            Geplant
+          </div>
+          <NuxtLink
+            :to="'/shipexkurs/' + shipData.slug"
+            class="m-0 transition hover:no-underline basis-full opacity-80 text-secondary hover:opacity-100"
+          >
+            {{ shipData.name }}
+            <span v-if="displayName && hangarData.userData.name"> - &quot;{{ hangarData.userData.name }}&quot;</span>
+          </NuxtLink>
+          <NuxtLink
+            :to="'/verseexkurs/companies/' + shipData.manufacturer.slug"
+            class="z-20 min-w-0 min-h-0 mt-auto text-xs text-white opacity-50 w-fit h-fit transition-group hover:no-underline hover:opacity-100"
+            :class="{ 'max-w-[calc(100%_-_60px)]': displayCrud }"
+            >{{ shipData.manufacturer.name }}
+          </NuxtLink>
+          <NuxtLink
+            v-if="displayOwner"
+            :to="(internalBio ? '/ams/employees/' : '/') + 'biography/' + hangarData.userData.owner.slug"
+            class="z-20 block mt-auto ml-auto text-xs text-white transition opacity-50 hover:no-underline hover:opacity-100"
+          >
+            <span>
+              <span :class="{ 'text-secondary': hangarData.userData.planned }">{{
+                hangarData.userData.planned ? 'Geplant' : 'Bereitgestell'
+              }}</span>
+              von: {{ hangarData.userData.owner.full_name }}</span
             >
-              <span>
-                <span :class="{ 'text-secondary': hangarData.userData.planned }">{{
-                  hangarData.userData.planned ? 'Geplant' : 'Bereitgestell'
-                }}</span>
-                von: {{ hangarData.userData.owner.fullName }}</span
-              >
-            </NuxtLink>
-            <div v-if="displayCrud" class="absolute z-20 block h-full mt-auto ml-auto right-4">
-              <div class="flex h-full pb-1 space-x-4 text-white/50">
+          </NuxtLink>
+          <div v-if="displayCrud" class="absolute z-20 block h-full mt-auto ml-auto right-4">
+            <div class="flex h-full pb-1 space-x-4 text-white/50">
+              <Icon
+                v-if="!hideEdit"
+                @click="handleEdit"
+                name="heroicons:pencil"
+                class="w-5 h-5 my-auto transition cursor-pointer hover:text-primary"
+              />
+              <UPopover :popper="{ placement: 'top-end' }" v-model:open="removePopover" class="w-5 h-5 my-auto">
                 <Icon
-                  v-if="!hideEdit"
-                  @click="handleEdit"
-                  name="heroicons:pencil"
-                  class="w-5 h-5 my-auto transition cursor-pointer hover:text-primary"
+                  @click="$emit('removeOpen')"
+                  name="heroicons:trash"
+                  class="w-full h-full transition cursor-pointer hover:text-danger"
                 />
-                <UPopover :popper="{ placement: 'top-end' }" :open="removePopover" class="w-5 h-5 my-auto">
-                  <Icon
-                    @click="
-                      removePopover = !removePopover;
-                      $emit('removeOpen');
-                    "
-                    name="heroicons:trash"
-                    class="w-full h-full transition cursor-pointer hover:text-danger"
-                  />
-                  <template #panel>
-                    <div class="p-4 text-xs">
-                      <p>Wollen sie das Schiff wirklich entfernen?</p>
-                      <div class="flex mx-auto mt-2 gap-x-4 w-fit">
-                        <ButtonDefault
-                          color="danger"
-                          @click="
-                            removePopover = false;
-                            $emit('removeConfirm', hangarData);
-                          "
-                        >
-                          <div class="flex gap-x-1">
-                            <Icon name="heroicons:trash" class="w-4 h-4 my-auto" />
-                            <span class="my-auto">Ja</span>
-                          </div>
-                        </ButtonDefault>
-                        <ButtonDefault @click="removePopover = false" color="success">
-                          <div class="flex">
-                            <span class="my-auto">Nein</span>
-                          </div>
-                        </ButtonDefault>
-                      </div>
+                <template #panel>
+                  <div class="p-4 text-xs">
+                    <p>Wollen sie das Schiff wirklich entfernen?</p>
+                    <div class="flex mx-auto mt-2 gap-x-4 w-fit">
+                      <ButtonDefault
+                        color="danger"
+                        @click="
+                          removePopover = false;
+                          $emit('removeConfirm', hangarData);
+                        "
+                      >
+                        <div class="flex gap-x-1">
+                          <Icon name="heroicons:trash" class="w-4 h-4 my-auto" />
+                          <span class="my-auto">Ja</span>
+                        </div>
+                      </ButtonDefault>
+                      <ButtonDefault @click="removePopover = false" color="success">
+                        <div class="flex">
+                          <span class="my-auto">Nein</span>
+                        </div>
+                      </ButtonDefault>
                     </div>
-                  </template>
-                </UPopover>
-              </div>
+                  </div>
+                </template>
+              </UPopover>
             </div>
           </div>
         </div>
-        <Presence :initial="!detailView" exit-before-enter>
-          <Motion
-            v-if="detailView"
-            :key="shipData.id + '-table'"
-            :initial="{ height: 0 }"
-            :animate="{ height: '260px' }"
-            :exit="{ height: 0 }"
-            :transition="{ duration: 0.5 }"
-            class="w-full px-1 ease-in-out overflow-clip transition-default bg-bprimary"
-          >
-            <div class="grid grid-cols-6 px-4 py-2 uppercase">
-              <TableRow title="Klassifizierung" :content="shipData.classification" />
-              <TableRow title="Crew" :content="(shipData.minCrew || 'N/A') + ' - ' + (shipData.maxCrew || 'N/A')" />
-              <TableRow title="Kaufpreis" :content="shipData.price && shipData.price + ' aUEC'" />
-              <TableRow title="Fracht" :content="shipData.cargo" />
-              <TableHr />
-              <TableRow title="Länge" :content="shipData.length && shipData.length + ' M'" third />
-              <TableRow title="Breite" :content="shipData.length && shipData.beam + ' M'" third />
-              <TableRow title="Höhe" :content="shipData.length && shipData.height + ' M'" third />
+      </div>
+      <Transition
+        enter-active-class="transition-all duration-500"
+        leave-active-class="transition-all duration-500"
+        enter-from-class="h-0"
+        :enter-to-class="hangarData ? 'h-[212px]' : 'h-[162px]'"
+        :leave-from-class="hangarData ? 'h-[212px]' : 'h-[162px]'"
+        leave-to-class="h-0"
+      >
+        <div
+          v-if="detailView"
+          :key="hangarData ? hangarData.id + '-table' : shipData.id + '-table'"
+          class="w-full px-1 ease-in-out overflow-clip transition-default bg-bprimary"
+        >
+          <div class="grid grid-cols-6 px-4 py-2 text-xs uppercase">
+            <TableRow title="Klassifizierung" :content="shipData.classification" />
+            <TableRow title="Crew" :content="(shipData.minCrew || 'N/A') + ' - ' + (shipData.maxCrew || 'N/A')" />
+            <TableRow title="Kaufpreis" :content="shipData.price && shipData.price + ' aUEC'" />
+            <TableRow title="Fracht" :content="shipData.cargo" />
+            <TableHr />
+            <TableRow title="Länge" :content="shipData.length && shipData.length + ' M'" third />
+            <TableRow title="Breite" :content="shipData.beam && shipData.beam + ' M'" third />
+            <TableRow title="Höhe" :content="shipData.height && shipData.height + ' M'" third />
+            <template v-if="hangarData">
               <TableHr />
               <TableRow title="Aktives Modul" :content="hangarData.userData.module?.name" full-width />
-            </div>
-          </Motion>
-        </Presence>
-      </DefaultPanel>
-    </Motion>
-  </Presence>
+            </template>
+          </div>
+        </div>
+      </Transition>
+    </DefaultPanel>
+  </div>
 </template>
 
 <style scoped lang="postcss">
