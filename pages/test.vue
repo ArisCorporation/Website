3bc2b7065e857203d41c8e1435bcbb7b06f46d4d
lucas-gruber <blaumc@icloud.com>
2024-04-17 14:18:16 +0200

    added test button for sentry

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,14 @@
 <script setup>
 const config = useRuntimeConfig();
 console.log(config);
+function throwError() {
+  throw new Error('Sentry Error');
+}
 </script>
 
 <template>
   <div>
-    <h1>CF TEST PAGE</h1>
+    <h1>TEST PAGE</h1>
+    <button @click="throwError">Throw error</button>
   </div>
 </template>
