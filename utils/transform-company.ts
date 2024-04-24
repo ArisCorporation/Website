e51356d3d7d245bc0a5c1ca45f1b7d573ff736e8
lucas-gruber <blaumc@icloud.com>
2024-04-24 18:06:08 +0200

    fix: fixed company transform

--- utils/transform-company.ts
+++ utils/transform-company.ts
@@ -10,31 +10,27 @@ export default function (obj: any) {
     ...(obj.category && { category: { id: obj.category.id, name: obj.category.name } }),
     ...(obj.headquarter && {
       headquarter: {
-        test: obj.headquarter[0],
         collection: obj.headquarter[0].collection,
-        ...(obj.headquarter[0].collection === 'planets' && {
-          planet: { name: obj.headquarter[0].headquarter.name, slug: obj.headquarter[0].headquarter.slug },
-        }),
-        ...(obj.headquarter[0].collection === 'moons' && {
-          moon: { name: obj.headquarter[0].headquarter.name, slug: obj.headquarter[0].headquarter.slug },
-        }),
-        ...(obj.headquarter[0].collection === 'landing_zones' && obj.headquarter[0].headquarter.planet
-          ? {
-              name: obj.headquarter[0].headquarter.name,
-              slug: obj.headquarter[0].headquarter.slug,
-              planet: {
-                name: obj.headquarter[0].headquarter.planet.name,
-                slug: obj.headquarter[0].headquarter.planet.slug,
-              },
-            }
-          : {}),
-        ...(obj.headquarter[0].collection === 'landing_zones' && obj.headquarter[0].headquarter.moon
-          ? {
-              name: obj.headquarter[0].headquarter.name,
-              slug: obj.headquarter[0].headquarter.slug,
-              moon: { name: obj.headquarter[0].headquarter.moon.name, slug: obj.headquarter[0].headquarter.moon.slug },
-            }
-          : {}),
+        ...(obj.headquarter[0].collection === 'planets' && { planet: transformPlanet(obj.headquarter[0].headquarter) }),
+        ...(obj.headquarter[0].collection === 'moons' && { moon: transformMoon(obj.headquarter[0].headquarter) }),
+        ...(obj.headquarter[0].collection === 'landing_zones' && transformLandingZone(obj.headquarter[0].headquarter)),
+        // ...(obj.headquarter[0].collection === 'landing_zones' && obj.headquarter[0].headquarter.planet
+        //   ? {
+        //       name: obj.headquarter[0].headquarter.name,
+        //       slug: obj.headquarter[0].headquarter.slug,
+        //       planet: {
+        //         name: obj.headquarter[0].headquarter.planet.name,
+        //         slug: obj.headquarter[0].headquarter.planet.slug,
+        //       },
+        //     }
+        //   : {}),
+        // ...(obj.headquarter[0].collection === 'landing_zones' && obj.headquarter[0].headquarter.moon
+        //   ? {
+        //       name: obj.headquarter[0].headquarter.name,
+        //       slug: obj.headquarter[0].headquarter.slug,
+        //       moon: { name: obj.headquarter[0].headquarter.moon.name, slug: obj.headquarter[0].headquarter.moon.slug },
+        //     }
+        //   : {}),
       },
     }),
     ...(obj.current_ceo && { current_ceo: obj.current_ceo }),
