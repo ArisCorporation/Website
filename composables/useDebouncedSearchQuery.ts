58517257449f8090edc3c1bb1d3714b17252b316
lucas-gruber <blaumc@icloud.com>
2024-06-01 23:30:15 +0200

    feat: new composable for handling debounced search with query

--- composables/useDebouncedSearchQuery.ts
+++ composables/useDebouncedSearchQuery.ts
@@ -2,11 +2,15 @@ import { watch } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import type { LocationQueryRaw } from 'vue-router';
 
-export const useDebouncedSearchQuery = (search: Ref<string>, search_input: Ref<string>): void => {
+export const useDebouncedSearchQuery = (
+  search: Ref<string>,
+  search_input: Ref<string>,
+  options?: { useQuery?: boolean; typingAction?: Function; debounceAction?: Function },
+): void => {
   const { replace } = useRouter();
   const { query } = useRoute();
 
-  if (query?.q) {
+  if (options?.useQuery !== false && query?.q) {
     search.value = query.q as string;
     search_input.value = query.q as string;
   }
@@ -14,12 +18,17 @@ export const useDebouncedSearchQuery = (search: Ref<string>, search_input: Ref<s
   const debounceSearch = useDebounce(() => {
     search.value = search_input.value;
 
-    replace({
-      query: search_input.value ? ({ q: search_input.value } as LocationQueryRaw) : undefined,
-    });
+    if (options?.useQuery !== false) {
+      replace({
+        query: search_input.value ? ({ q: search_input.value } as LocationQueryRaw) : undefined,
+      });
+    }
+
+    options?.debounceAction?.();
   }, 500);
 
   watch(search_input, () => {
+    options?.typingAction?.();
     debounceSearch();
   });
 };
