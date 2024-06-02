3723b630d798bcfdf938de067c7b1020151e9354
lucas-gruber <blaumc@icloud.com>
2024-06-02 11:06:34 +0200

    feat: added to-prop to defaultButton

--- components/global/button/default.vue
+++ components/global/button/default.vue
@@ -20,11 +20,17 @@ defineProps({
     required: false,
     default: 'base',
   },
+  to: {
+    type: String,
+    required: false,
+    default: null,
+  },
 });
 </script>
 
 <template>
   <button
+    v-if="!to"
     class="relative inline-block group/button after:absolute after:bg-bsecondary after:left-4 after:right-4 before:absolute before:bg-bsecondary before:box-border before:left-4 before:right-4 bg-bprimary text-inherit"
     :class="[
       {
@@ -50,6 +56,34 @@ defineProps({
       <p class="p-0"><slot /></p>
     </div>
   </button>
+  <NuxtLink
+    v-else
+    :to="to"
+    class="relative inline-block group/button after:absolute after:bg-bsecondary after:left-4 after:right-4 before:absolute before:bg-bsecondary before:box-border before:left-4 before:right-4 bg-bprimary text-inherit"
+    :class="[
+      {
+        'animate-link': !noAnimation && !disabled,
+        'p-0.5 rounded-[10px] border-2 after:h-0.5 before:h-0.5 before:-top-0.5 after:-bottom-0.5': size === 'base',
+        'p-[2px] text-xs rounded-lg border after:h-px before:h-px before:-top-px after:-bottom-px': size === 'xs',
+      },
+      disabled ? 'grayscale opacity-50 cursor-not-allowed' : 'cursor-pointer border-' + color,
+      ,
+    ]"
+  >
+    <div
+      class="box-border overflow-hidden text-center transition-all duration-200 group-hover/button:duration-300 whitespace-nowrap text-ellipsis"
+      :class="[
+        {
+          'group-hover/button:bg-btertiary': !disabled,
+          'p-2 rounded-md': size === 'base',
+          'p-1 rounded-[5px]': size === 'xs',
+        },
+        'group-hover:text-' + color,
+      ]"
+    >
+      <p class="p-0"><slot /></p>
+    </div>
+  </NuxtLink>
 </template>
 <!-- text-industrial-400 -->
 <!-- border-industrial-400 -->
