2c5cdf28977c5a976d1fd12f6a02fcba4bf0a5e6
lucas-gruber <blaumc@icloud.com>
2024-06-11 21:17:30 +0200

    ref: refactored auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,27 +1,17 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
-  const { user, setUser } = useDirectusAuth();
+  const { refreshTokenCookie, refresh, tokens, readMe, user } = useDirectusAuth({ staticToken: false });
+  const nuxtApp = useNuxtApp();
+  const queryString = Object.entries(to.query)
+    .filter(([key]) => key !== 'redirect')
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+
+  const fullPath = to.path + (queryString ? `?${queryString}` : '');
+
   if (!user.value) {
-    /*
-      const path = '/ams/login';
-      const queries = {
-        redirect: "/ams",
-        pw: 'dsaodaps'
-      };
-
-      const queryString = Object.entries(queries)
-        .map(([key, value]) => `${key}=${value}`)
-        .join('&');
-
-      const fullPath = path + (queryString ? `?${queryString}` : '');
-    */
-    const queryString = Object.entries(to.query)
-      .filter(([key]) => key !== 'redirect')
-      .map(([key, value]) => `${key}=${value}`)
-      .join('&');
-
-    const fullPath = to.path + (queryString ? `?${queryString}` : '');
-
-    if (to.path !== '/login') {
+    if (!nuxtApp.isHydrating) {
+      return abortNavigation();
+    } else {
       return navigateTo({
         path: '/ams/login',
         query: {
@@ -30,4 +20,35 @@ export default defineNuxtRouteMiddleware(async (to, _from) => {
       });
     }
   }
+
+  // if (!user.value) {
+  //   /*
+  //     const path = '/ams/login';
+  //     const queries = {
+  //       redirect: "/ams",
+  //       pw: 'dsaodaps'
+  //     };
+
+  //     const queryString = Object.entries(queries)
+  //       .map(([key, value]) => `${key}=${value}`)
+  //       .join('&');
+
+  //     const fullPath = path + (queryString ? `?${queryString}` : '');
+  //   */
+  //   const queryString = Object.entries(to.query)
+  //     .filter(([key]) => key !== 'redirect')
+  //     .map(([key, value]) => `${key}=${value}`)
+  //     .join('&');
+
+  //   const fullPath = to.path + (queryString ? `?${queryString}` : '');
+
+  //   if (to.path !== '/login') {
+  //     return navigateTo({
+  //       path: '/ams/login',
+  //       query: {
+  //         redirect: encodeURIComponent(fullPath),
+  //       },
+  //     });
+  //   }
+  // }
 });
