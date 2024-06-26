00f803cac83df2570c66f6db3fabde1b0c68b4a4
lucas-gruber <blaumc@icloud.com>
2024-06-26 13:09:03 +0200

    ref: reactored auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,6 @@
-export default defineNuxtRouteMiddleware(async (to, _from) => {
-  const { refreshTokenCookie, refresh, tokens, readMe, user } = useDirectusAuth({ staticToken: false });
+export default defineNuxtRouteMiddleware((to, _from) => {
+  const { refreshTokenCookie, refresh, tokens, readMe, user } = useDirectusAuth();
   const nuxtApp = useNuxtApp();
-  const queryString = Object.entries(to.query)
-    .filter(([key]) => key !== 'redirect')
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
-
-  const fullPath = to.path + (queryString ? `?${queryString}` : '');
 
   if (!user.value) {
     if (nuxtApp.isHydrating) {
@@ -15,40 +9,9 @@ export default defineNuxtRouteMiddleware(async (to, _from) => {
       return navigateTo({
         path: '/ams/login',
         query: {
-          redirect: encodeURIComponent(fullPath),
+          redirect: to.fullPath,
         },
       });
     }
   }
-
-  // if (!user.value) {
-  //   /*
-  //     const path = '/ams/login';
-  //     const queries = {
-  //       redirect: "/ams",
-  //       pw: 'dsaodaps'
-  //     };
-
-  //     const queryString = Object.entries(queries)
-  //       .map(([key, value]) => `${key}=${value}`)
-  //       .join('&');
-
-  //     const fullPath = path + (queryString ? `?${queryString}` : '');
-  //   */
-  //   const queryString = Object.entries(to.query)
-  //     .filter(([key]) => key !== 'redirect')
-  //     .map(([key, value]) => `${key}=${value}`)
-  //     .join('&');
-
-  //   const fullPath = to.path + (queryString ? `?${queryString}` : '');
-
-  //   if (to.path !== '/login') {
-  //     return navigateTo({
-  //       path: '/ams/login',
-  //       query: {
-  //         redirect: encodeURIComponent(fullPath),
-  //       },
-  //     });
-  //   }
-  // }
 });
