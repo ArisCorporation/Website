9be0622b1eed9141bc83f5a87b287bced092a62d
lucas-gruber <blaumc@icloud.com>
2024-06-28 12:52:09 +0200

    test10

--- composables/useSlugify.ts
+++
@@ -1,6 +0,0 @@
-export const useSlugify = (string: string, dotMode?: boolean) => {
-  return string
-    .toLowerCase()
-    .replace(/ /g, dotMode === true ? '.' : '-')
-    .replace(dotMode ? /[^\w.]+/g : /[^\w-]+/g, '');
-};
