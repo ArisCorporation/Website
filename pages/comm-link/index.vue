0f1a365189f3bc7fea3055c584ccdf2ae71176a9
lucas-gruber <blaumc@icloud.com>
2024-07-01 11:39:26 +0200

    feat: added pagination to comm-link

--- pages/comm-link/index.vue
+++ pages/comm-link/index.vue
@@ -1,123 +1,142 @@
 <script setup lang="ts">
-const { readAsyncItems } = useDirectusItems();
-const selectedChannel = ref('Alle');
-const search = ref('');
+const { readAsyncItems } = useDirectusItems()
+const selectedChannel = ref('Alle')
+const search = ref('')
+const search_input_value = ref('')
+
+const page = ref(1)
+const pageCount = ref(10)
+const pageTotal = ref(0)
+const pageFrom = computed(() => (page.value - 1) * pageCount.value + 1)
+const pageTo = computed(() => Math.min(page.value * pageCount.value, pageTotal.value))
+
+const filters = reactive({
+	status: { _eq: 'published' },
+	...useDirectusSearch(search.value, ['name', 'user_created.first_name', 'user_created.last_name']),
+})
+
+watch(search, () => {
+	filters._or = useDirectusSearch(search.value, ['name', 'user_created.first_name', 'user_created.last_name'])._or
+})
+
+const { data: commLinksCount } = await readAsyncItems('comm_links', {
+	query: {
+		fields: ['id'],
+		filter: filters,
+		limit: -1,
+		sort: ['-date_created'],
+	},
+	watch: [search, page, pageCount, selectedChannel],
+})
+watch(
+	[commLinksCount],
+	() => {
+		if (commLinksCount.value) {
+			pageTotal.value = commLinksCount.value.length
+		}
+	},
+	{ immediate: true },
+)
 
 const { data: commLinks } = await readAsyncItems('comm_links', {
-  query: {
-    fields: ['id', 'name', 'banner', 'description', 'channel.name', 'channel.description', 'date_created'],
-    filter: {
-      status: { _eq: 'published' },
-    },
-    limit: -1,
-    sort: ['-date_created'],
-  },
-});
+	query: {
+		fields: ['id', 'name', 'banner', 'description', 'channel.name', 'channel.description', 'date_created'],
+		filter: filters,
+		limit: pageCount,
+		page,
+		sort: ['-date_created'],
+	},
+	watch: [search, page, pageCount, selectedChannel],
+})
 
 const { data: channels } = await readAsyncItems('comm_link_channels', {
-  query: {
-    fields: ['id', 'name', 'description'],
-    limit: -1,
-    sort: ['name'],
-  },
-  transform: (channels: any[]) => channels.map((channel) => channel.name),
-});
-// const channels = computed(() => channelsRes.map((obj: any) => obj.name));
+	query: {
+		fields: ['id', 'name', 'description'],
+		limit: -1,
+		sort: ['name'],
+	},
+	transform: (channels: any[]) => channels.map(channel => channel.name),
+})
 
-// const { data } = await useAsyncData('comm-link-data', async () => {
-//   const [commLinks, channels] = await Promise.all([
-//     getItems({
-//       collection: 'comm_links',
-//       params: {
-//         fields: [
-//           'id',
-//           'comm_link_titel',
-//           'comm_link_banner.id',
-//           'comm_link_channel.channel',
-//           'comm_link_channel.beschreibung',
-//           'date_created',
-//         ],
-//         filter: {
-//           status: { _eq: 'published' },
-//         },
-//         limit: -1,
-//         sort: ['-date_created'],
-//       },
-//     }),
-//     getItems({
-//       collection: 'commm_link_channel',
-//       params: {
-//         fields: ['id', 'channel', 'beschreibung'],
-//         limit: -1,
-//         sort: ['channel'],
-//       },
-//     }),
-//   ]);
+useSearch(search, search_input_value, {
+	debounce: true,
+	query: true,
+})
 
-//   if (!commLinks || !channels) {
-//     return null;
-//   }
+const filteredCommLinks = ref(commLinks.value.map((obj: any) => transformCommLink(obj, commLinks.value)))
+watch(commLinks, () => filteredCommLinks.value = commLinks.value.map((obj: any) => transformCommLink(obj, commLinks.value)))
 
-//   return {
-//     commLinks,
-//     channels: channels.map((obj) => obj.channel),
-//   };
-// });
-
-if (!commLinks.value || !channels.value) {
-  throw createError({
-    statusCode: 500,
-    statusMessage: 'Es konnten nicht alle Daten vollständig empfangen werden!',
-    fatal: true,
-  });
-}
-
-useSearchQuery(search);
-
-const filterData = computed(() =>
-  commLinks.value.filter((e) =>
-    selectedChannel.value !== 'Alle' ? e.comm_link_channel?.channel === selectedChannel.value : e,
-  ),
-);
-
-const filteredCommLinks = computed(() => {
-  return filterData.value
-    ?.filter((e: any) => (search.value ? e.name.toLowerCase().includes(search.value.toLowerCase()) : e))
-    .map((obj: any) => transformCommLink(obj, filterData.value));
-});
+onMounted(() => watch(page, () => window.scrollTo(0, 0)))
 
 definePageMeta({
-  layout: 'default',
-});
+	layout: 'default',
+})
 useHead({
-  title: 'Comm-Link',
-});
+	title: 'Comm-Link',
+})
 </script>
+
 <template>
-  <div>
-    <div>
-      <NuxtImg src="2f7590ef-c84a-495c-8acc-93653f0cddd3" :placeholder="[16, 16, 1, 5]" class="w-full mx-auto" />
-      <hr />
-      <div class="w-full sm:flex sm:space-x-12">
-        <div class="w-full">
-          <label for="channelSelect">Channel:</label>
-          <USelectMenu
-            id="channelSelect"
-            v-model="selectedChannel"
-            name="Channel"
-            placeholder="Channel filtern"
-            :options="['Alle', ...channels]"
-            size="md"
-          />
-        </div>
-        <UInput v-model="search" size="md" placeholder="Suche..." class="w-full mt-4 sm:mt-auto" />
-      </div>
-      <div class="my-6">
-        <TableHr />
-      </div>
-    </div>
-    <div class="comm-link-grid">
-      <CommLinkCard v-for="commLink in filteredCommLinks" :key="commLink.id" :data="commLink" :size="commLink.size" />
-    </div>
-  </div>
+	<div>
+		<div>
+			<NuxtImg
+				src="2f7590ef-c84a-495c-8acc-93653f0cddd3"
+				:placeholder="[16, 16, 1, 5]"
+				class="w-full h-auto mx-auto aspect-[1118/351]"
+			/>
+			<hr>
+			<div class="w-full sm:flex sm:space-x-12">
+				<div class="w-full">
+					<label for="channelSelect">Channel:</label>
+					<USelectMenu
+						id="channelSelect"
+						v-model="selectedChannel"
+						name="Channel"
+						placeholder="Channel filtern"
+						:options="['Alle', ...channels]"
+						size="md"
+					/>
+				</div>
+				<UInput
+					v-model="search_input_value"
+					size="md"
+					placeholder="Suche..."
+					class="w-full mt-4 sm:mt-auto"
+				/>
+			</div>
+			<div class="my-6">
+				<TableHr />
+			</div>
+		</div>
+		<div class="comm-link-grid">
+			<CommLinkCard
+				v-for="commLink in filteredCommLinks"
+				:key="commLink.id"
+				:data="commLink"
+				:size="commLink.size"
+			/>
+		</div>
+		<div class="col-span-3 py-2">
+			<div class="mx-auto mb-2 text-center w-fit">
+				<div class="flex justify-center">
+					<UPagination
+						v-model="page"
+						:page-count="pageCount"
+						:total="pageTotal"
+					/>
+				</div>
+				<div>
+					<span class="text-sm leading-5">
+						Zeigt
+						<span class="font-medium">{{ pageFrom }}</span>
+						bis
+						<span class="font-medium">{{ pageTo }}</span>
+						von
+						<span class="font-medium">{{ pageTotal }}</span>
+						Ergebnissen
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
