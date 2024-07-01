1b548ea05dd7a968b455fe218e6e00e04995e436
lucas-gruber <blaumc@icloud.com>
2024-07-01 11:39:06 +0200

    ref: refactored code

--- pages/verseexkurs/fractions/index.vue
+++ pages/verseexkurs/fractions/index.vue
@@ -1,135 +1,151 @@
 <script setup lang="ts">
-const { readItems } = useDirectusItems();
-const selectedMainTab = ref(0);
-const selectedSubTab = ref(0);
+const { readItems } = useDirectusItems()
+const selectedMainTab = ref(0)
+const selectedSubTab = ref(0)
 
 const categories = await readItems('fraction_categories', {
-  fields: ['name', 'sub_categories.name', 'sub_categories.sub_categories.name'],
-  filter: { superior_category: { _null: true } },
-  sort: ['sort'],
-});
+	fields: ['name', 'sub_categories.name', 'sub_categories.sub_categories.name'],
+	filter: { superior_category: { _null: true } },
+	sort: ['sort'],
+})
 
 const fractions = await readItems('fractions', {
-  fields: ['name', 'slug', 'logo', 'category.name'],
-  filter: { status: { _eq: 'published' } },
-  sort: ['name'],
-  limit: -1,
-});
+	fields: ['name', 'slug', 'logo', 'category.name'],
+	filter: { status: { _eq: 'published' } },
+	sort: ['name'],
+	limit: -1,
+})
 
 if (!categories || !fractions) {
-  throw createError({
-    statusCode: 500,
-    statusMessage: 'Die Übertragung konnte nicht vollständig empfangen werden!',
-    fatal: true,
-  });
+	throw createError({
+		statusCode: 500,
+		statusMessage: 'Die Übertragung konnte nicht vollständig empfangen werden!',
+		fatal: true,
+	})
 }
 
-const mainCategory = computed(() => categories[selectedMainTab.value]);
-const subCategories = computed(() => categories[selectedMainTab.value].sub_categories);
+const mainCategory = computed(() => categories[selectedMainTab.value])
+const subCategories = computed(() => categories[selectedMainTab.value].sub_categories)
 const subSubCategories = computed(
-  () => categories[selectedMainTab.value].sub_categories[selectedSubTab.value].sub_categories,
-);
+	() => categories[selectedMainTab.value].sub_categories[selectedSubTab.value].sub_categories,
+)
 
 const subTabs = computed(() =>
-  categories[selectedMainTab.value].sub_categories.map((obj) => ({
-    header: obj.name,
-  })),
-);
+	categories[selectedMainTab.value].sub_categories.map(obj => ({
+		header: obj.name,
+	})),
+)
 
 const changeMainTab = async (index: number) => {
-  selectedSubTab.value = 0;
-  selectedMainTab.value = index;
-};
+	selectedSubTab.value = 0
+	selectedMainTab.value = index
+}
 
 useHead({
-  title: 'Firmen',
-});
+	title: 'Fraktionen',
+})
 definePageMeta({
-  layout: 'verse-exkurs',
-});
+	layout: 'verse-exkurs',
+})
 </script>
 
 <template>
-  <div>
-    <TabGroup
-      :tablist="
-        categories.map((obj) => ({
-          header: obj.name,
-        }))
-      "
-      between
-      :store="selectedMainTab"
-      :change="changeMainTab"
-    >
-      <template #tabcontent>
-        <TabGroup
-          v-if="subCategories[0]"
-          :tablist="subTabs"
-          hide-hr
-          small-header
-          between
-          no-margin
-          :store="selectedSubTab"
-          :change="(index: number) => (selectedSubTab = index)"
-        >
-          <template #tabcontent>
-            <div v-for="category in subSubCategories" v-if="subSubCategories[0]" :key="category.name">
-              <TableHr
-                v-if="
-                  fractions.filter(
-                    (obj: any) => obj.category.name === subSubCategories.find((obj) => obj.name === category.name).name,
-                  )[0]
-                "
-              >
-                {{ category.name }}
-              </TableHr>
-              <div class="fraction-grid">
-                <NuxtLink
-                  v-for="fraction in fractions
-                    .filter(
-                      (obj: any) =>
-                        obj.category.name === subSubCategories.find((obj) => obj.name === category.name).name,
-                    )
-                    .sort((a, b) => a.name.localeCompare(b.name))"
-                  :key="fraction.slug"
-                  :to="'/verseexkurs/fractions/' + fraction.slug"
-                  class="w-full h-full transition opacity-50 hover:opacity-100 animate-link"
-                >
-                  <ImageHoverEffect :src="fraction.logo" class="w-full h-full" />
-                </NuxtLink>
-              </div>
-            </div>
-            <div v-else class="fraction-grid">
-              <NuxtLink
-                v-for="fraction in fractions
-                  .filter((obj: any) => obj.category.name === subCategories[selectedSubTab].name)
-                  .sort((a, b) => a.name.localeCompare(b.name))"
-                :key="fraction.slug"
-                :to="'/verseexkurs/fractions/' + fraction.slug"
-                class="w-full h-full transition opacity-50 hover:opacity-100 animate-link"
-              >
-                <ImageHoverEffect :src="fraction.logo" class="w-full h-full" />
-              </NuxtLink>
-            </div>
-          </template>
-        </TabGroup>
-        <template v-else>
-          <div class="fraction-grid">
-            <NuxtLink
-              v-for="fraction in fractions
-                .filter((obj: any) => obj.category.name === mainCategory.name)
-                .sort((a, b) => a.name.localeCompare(b.name))"
-              :key="fraction.slug"
-              :to="'/verseexkurs/fractions/' + fraction.slug"
-              class="w-full h-full transition opacity-50 hover:opacity-100 animate-link"
-            >
-              <ImageHoverEffect :src="fraction.logo" class="w-full h-full" />
-            </NuxtLink>
-          </div>
-        </template>
-      </template>
-    </TabGroup>
-  </div>
+	<div>
+		<TabGroup
+			:tablist="
+				categories.map((obj) => ({
+					header: obj.name,
+				}))
+			"
+			between
+			:store="selectedMainTab"
+			:change="changeMainTab"
+		>
+			<template #tabcontent>
+				<TabGroup
+					v-if="subCategories[0]"
+					:tablist="subTabs"
+					hide-hr
+					small-header
+					between
+					no-margin
+					:store="selectedSubTab"
+					:change="(index: number) => (selectedSubTab = index)"
+				>
+					<template #tabcontent>
+						<div
+							v-for="category in subSubCategories"
+							v-if="subSubCategories[0]"
+							:key="category.name"
+						>
+							<TableHr
+								v-if="
+									fractions.filter(
+										(obj: any) => obj.category.name === subSubCategories.find((obj) => obj.name === category.name).name,
+									)[0]
+								"
+							>
+								{{ category.name }}
+							</TableHr>
+							<div class="fraction-grid">
+								<NuxtLink
+									v-for="fraction in fractions
+										.filter(
+											(obj: any) =>
+												obj.category.name === subSubCategories.find((obj) => obj.name === category.name).name,
+										)
+										.sort((a, b) => a.name.localeCompare(b.name))"
+									:key="fraction.slug"
+									:to="'/verseexkurs/fractions/' + fraction.slug"
+									class="w-full h-full transition opacity-50 hover:opacity-100 animate-link"
+								>
+									<ImageHoverEffect
+										:src="fraction.logo"
+										class="w-full h-full"
+									/>
+								</NuxtLink>
+							</div>
+						</div>
+						<div
+							v-else
+							class="fraction-grid"
+						>
+							<NuxtLink
+								v-for="fraction in fractions
+									.filter((obj: any) => obj.category.name === subCategories[selectedSubTab].name)
+									.sort((a, b) => a.name.localeCompare(b.name))"
+								:key="fraction.slug"
+								:to="'/verseexkurs/fractions/' + fraction.slug"
+								class="w-full h-full transition opacity-50 hover:opacity-100 animate-link"
+							>
+								<ImageHoverEffect
+									:src="fraction.logo"
+									class="w-full h-full"
+								/>
+							</NuxtLink>
+						</div>
+					</template>
+				</TabGroup>
+				<template v-else>
+					<div class="fraction-grid">
+						<NuxtLink
+							v-for="fraction in fractions
+								.filter((obj: any) => obj.category.name === mainCategory.name)
+								.sort((a, b) => a.name.localeCompare(b.name))"
+							:key="fraction.slug"
+							:to="'/verseexkurs/fractions/' + fraction.slug"
+							class="w-full h-full transition opacity-50 hover:opacity-100 animate-link"
+						>
+							<ImageHoverEffect
+								:src="fraction.logo"
+								class="w-full h-full"
+							/>
+						</NuxtLink>
+					</div>
+				</template>
+			</template>
+		</TabGroup>
+	</div>
 </template>
 
 <style scoped lang="postcss">
