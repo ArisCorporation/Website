e39ab88c49f5f770ebd098b6463fc148d0aac4c2
lucas-gruber <blaumc@icloud.com>
2024-10-02 19:08:11 +0200

    added coming soon in profile notifications

--- pages/ams/profile/privacy.vue
+++ pages/ams/profile/privacy.vue
@@ -1,3 +1,4 @@
 <template>
-  <h1>priv, fleet & hangar quick view on, off; hangar öffentlich anzeigen on, off</h1>
+  <!-- <h1>priv, fleet & hangar quick view on, off; hangar öffentlich anzeigen on, off</h1> -->
+  <h1>COMING SOON</h1>
 </template>
