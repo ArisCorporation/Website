553ade475a013991bb9dd8c2c40e771b99b269da
Lucas Gruber <gruber.lucas05@gmail.com>
2025-04-21 10:04:40 +0200

    feat: configured modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   modules: [
     '@nuxt/eslint',
-    '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/test-utils',
     '@nuxt/ui',
     '@pinia/nuxt',
     '@vueuse/nuxt'
-  ]
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  ui: {
+    fonts: true
+  },
+
+  image: {
+    provider: 'directus',
+    directus: {
+      baseURL: process.env.NUXT_PUBLIC_FILE_BASE,
+      modifiers: {
+        format: 'webp',
+      },
+    },
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-11-01',
+
 })
