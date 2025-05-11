f9e1386f5ac67622763ccc5c9c745fb8cb3e1db6
lucas-gruber <blaumc@icloud.com>
2025-05-11 12:24:40 +0200

    finalized distribution step

--- layers/ams/app/components/pages/calculator/steps/distribution.vue
+++ layers/ams/app/components/pages/calculator/steps/distribution.vue
@@ -1,59 +1,92 @@
 <script setup lang="ts">
-import type { Distribution } from '@@/types/ams-calculator'
+const store = useAMSCalculatorStore()
+const { settings, workers, incomes, expenses, distribution } =
+  storeToRefs(store)
 
-const distributionsMock = reactive<Distribution[]>([
-  {
-    id: '1',
-    worker: '1',
-    expenses: 10000,
-    share: 10000,
-    percentage: 50,
-  },
-  {
-    id: '2',
-    worker: '2',
-    expenses: 10000,
-    share: 10000,
-    percentage: 50,
-  },
-])
+const calculated = ref(false)
+const calculating = ref(false)
+
+async function calculate() {
+  calculating.value = true
+  await setTimeout(() => {
+    const distributionData = amsCalculateDistribution(
+      incomes.value,
+      expenses.value,
+      workers.value,
+      settings.value
+    )
+
+    distribution.value = distributionData
+    calculating.value = false
+    calculated.value = true
+    console.log(distributionData)
+  }, 1000)
+}
 </script>
 
 <template>
   <div class="space-y-6">
     <div class="space-y-4">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div class="grid grid-cols-1 md:grid-cols-6 gap-4">
         <AMSPagesCalculatorExpertDistributionCurrencyCard
           title="Gesamteinnahmen"
-          :value="formatCurrency(300000)"
+          :value="formatCurrency(distribution?.totalGrossRawIncome)"
           icon="i-lucide-dollar-sign"
           color="green"
+          class="col-span-2"
         />
         <AMSPagesCalculatorExpertDistributionCurrencyCard
           title="Gesamtausgaben"
-          :value="formatCurrency(300000)"
+          :value="formatCurrency(distribution?.totalMissionExpenses)"
           icon="i-lucide-dollar-sign"
           color="red"
+          class="col-span-2"
         />
-        <!-- TODO: add v-if - only when there is a transaction fee -->
         <AMSPagesCalculatorExpertDistributionCurrencyCard
-          :title="`Transaktionsgebühr (TBD)`"
-          :value="formatCurrency(300000)"
+          v-if="settings.fee_enabled"
+          :title="`Transaktionsgebühr (${settings.fee_percentage}%)`"
+          :value="formatCurrency(distribution?.totalAllTransactionFees)"
           icon="i-lucide-credit-card"
           color="orange"
+          class="col-span-2"
         />
         <AMSPagesCalculatorExpertDistributionCurrencyCard
           title="Nettogewinn"
-          :value="formatCurrency(300000)"
+          :value="formatCurrency(distribution?.netOperatingProfit)"
+          icon="i-lucide-calculator"
+          class="col-span-3"
+        />
+        <AMSPagesCalculatorExpertDistributionCurrencyCard
+          title="Nettogewinn pro Mitarbeiter"
+          :value="
+            formatCurrency(distribution?.targetNetProfitShareForEachWorker)
+          "
           icon="i-lucide-calculator"
           class="col-span-3"
         />
       </div>
-    </div>
-    <div class="space-y-4">
-      <AMSPagesCalculatorExpertDistributionTable
-        :distributions="distributionsMock"
-      />
+      <USeparator color="ams" />
+      <div class="space-y-4">
+        <div
+          v-if="!calculated"
+          class="flex flex-col items-center justify-center py-8 space-y-4 text-center"
+        >
+          <UIcon
+            name="i-lucide-calculator"
+            class="size-12 text-(--ui-primary)"
+          />
+          <p class="text-(--ui-text-muted)">
+            Klicke auf "Verteilung berechnen", um die Anteile zu berechnen
+          </p>
+          <UButton
+            @click="calculate"
+            :loading="calculating"
+            icon="i-lucide-calculator"
+            label="Verteilung berechnen"
+          />
+        </div>
+        <AMSPagesCalculatorTablesDistribution v-else />
+      </div>
     </div>
   </div>
 </template>
