d89c2c73ca977553c6c165e1e94bffbed61d66f0
lucas-gruber <blaumc@icloud.com>
2025-05-11 12:24:08 +0200

    finalized settings step

--- layers/ams/app/components/pages/calculator/steps/settings.vue
+++ layers/ams/app/components/pages/calculator/steps/settings.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const store = useAMSCalculatorStore()
+const { settings } = storeToRefs(store)
+</script>
+
 <template>
   <div class="space-y-2">
     <UFormField>
@@ -8,6 +13,7 @@
         </div>
       </template>
       <UInput
+        v-model="settings.name"
         highlight
         variant="subtle"
         class="w-full"
@@ -21,19 +27,28 @@
           <span>Transaktionsgebühr abziehen?</span>
         </div>
       </template>
-      <USwitch size="xl" :model-value="true" />
+      <USwitch v-model="settings.fee_enabled" size="xl" />
     </UFormField>
-    <UFormField class="mt-4">
+    <UFormField v-if="settings.fee_enabled" class="mt-4">
       <template #label>
         <div class="flex items-center space-x-2">
           <UIcon name="i-lucide-dollar-sign" class="text-(--ui-primary)" />
           <span>Gebührenprozentsatz</span>
         </div>
       </template>
-      <USlider size="xl" :min="0.5" :max="20" :step="0.5" class="py-4" />
+      <USlider
+        v-model="settings.fee_percentage"
+        size="xl"
+        :min="0.5"
+        :max="20"
+        :step="0.5"
+        class="py-4"
+      />
       <div class="flex justify-between items-center">
         <span class="text-sm text-(--ui-text-muted)">0%</span>
-        <span class="text-sm text-(--ui-primary)">TBD%</span>
+        <span class="text-sm text-(--ui-primary)">
+          {{ settings.fee_percentage }}%
+        </span>
         <span class="text-sm text-(--ui-text-muted)">20%</span>
       </div>
     </UFormField>
