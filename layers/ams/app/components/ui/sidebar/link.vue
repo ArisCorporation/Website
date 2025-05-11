e1517f78345c8bb81a428bdd43161d963fefafee
lucas-gruber <blaumc@icloud.com>
2025-05-11 18:29:27 +0200

    fixed a tags and sidebar links

--- layers/ams/app/components/ui/sidebar/link.vue
+++ layers/ams/app/components/ui/sidebar/link.vue
@@ -32,6 +32,6 @@ const active = computed(() =>
       class="mr-3 h-5 w-5 transition-transform duration-300 group-hover:scale-110"
     />
     <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layout-grid "><rect width="7" height="7" x="3" y="3" rx="1"></rect><rect width="7" height="7" x="14" y="3" rx="1"></rect><rect width="7" height="7" x="14" y="14" rx="1"></rect><rect width="7" height="7" x="3" y="14" rx="1"></rect></svg> -->
-    <span class="transition-colors duration-300">{{ label }}</span>
+    <span>{{ label }}</span>
   </NuxtLink>
 </template>
