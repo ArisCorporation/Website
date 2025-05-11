a80648ebb96ce329b44f736564b7e59bee0c6637
lucas-gruber <blaumc@icloud.com>
2025-05-11 12:24:48 +0200

    finalized distribution view for expert mode

--- layers/ams/app/components/pages/calculator/expert/distribution.vue
+++ layers/ams/app/components/pages/calculator/expert/distribution.vue
@@ -1,35 +1,14 @@
-<script setup lang="ts">
-import type { Distribution } from '@@/types/ams-calculator'
-
-const distributionsMock = reactive<Distribution[]>([
-  {
-    id: '1',
-    worker: '1',
-    expenses: 10000,
-    share: 10000,
-    percentage: 50,
-  },
-  {
-    id: '2',
-    worker: '2',
-    expenses: 10000,
-    share: 10000,
-    percentage: 50,
-  },
-])
-</script>
-
 <template>
   <div class="mt-4 space-y-6">
-    <AMSUiCard class="bg-(--ui-bg-muted)/50">
-      <AMSUiCardHeader class="pb-3">
-        <AMSUiCardTitle class="text-(--ui-primary)">
-          Verteilung
-        </AMSUiCardTitle>
-      </AMSUiCardHeader>
-      <AMSUiCardContent class="space-y-6">
-        <AMSPagesCalculatorStepsDistribution />
-      </AMSUiCardContent>
-    </AMSUiCard>
+    <UCard variant="ams">
+      <template #header>
+        <h2>Verteilung</h2>
+      </template>
+      <template #default>
+        <div class="space-y-4">
+          <AMSPagesCalculatorStepsDistribution />
+        </div>
+      </template>
+    </UCard>
   </div>
 </template>
