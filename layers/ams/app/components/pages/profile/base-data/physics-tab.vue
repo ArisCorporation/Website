c39f09b5d6ff78d48b830156a18c45cee2c1bf7b
lucas-gruber <blaumc@icloud.com>
2025-06-01 23:10:12 +0200

    added medical informations

--- layers/ams/app/components/pages/profile/base-data/physics-tab.vue
+++ layers/ams/app/components/pages/profile/base-data/physics-tab.vue
@@ -54,5 +54,20 @@ const profileEdit = useUserProfileEditStore()
         class="w-full"
       />
     </UFormField>
+    <UFormField
+      label="Medizinisch relevante Informationen?"
+      name="medical_information"
+      size="xs"
+      class="w-full col-span-2"
+    >
+      <UTextarea
+        v-model="profileEdit.formData.medical_informations"
+        :rows="12"
+        class="w-full"
+        :ui="{
+          base: 'ring-(--ui-primary)/20 bg-transparent',
+        }"
+      />
+    </UFormField>
   </div>
 </template>
