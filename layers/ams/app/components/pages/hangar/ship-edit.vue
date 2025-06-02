c7a00f45c1ab9b0b7e426b36d34686dd56ce7b4a
Lucas Gruber <gruber.lucas05@gmail.com>
2025-06-02 11:15:28 +0200

    fix: fixed fleet editing

--- layers/ams/app/components/pages/hangar/ship-edit.vue
+++ layers/ams/app/components/pages/hangar/ship-edit.vue
@@ -7,7 +7,7 @@ import type {
   UserHangar,
 } from '~~/types'
 
-const props = defineProps<{ item: UserHangar }>()
+const props = defineProps<{ item: UserHangar; fleetMode: boolean }>()
 
 const store = useHangarItemEditStore()
 const authStore = useAuthStore()
@@ -35,8 +35,11 @@ async function handleEditSubmit() {
   await setTimeout(async () => {
     await store.submitHangarItem()
 
-    const { refresh } = await useFetchAMSHangar(userId)
-    refresh()
+    const { refresh: refreshHangar } = await useFetchAMSHangar(userId)
+    const { refresh: refreshFleet } = await useFetchAMSFleet()
+
+    if (props.fleetMode) refreshFleet()
+    else refreshHangar()
   }, 300)
 }
 
