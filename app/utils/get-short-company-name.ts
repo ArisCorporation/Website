fb552d51ab20dfb3a671c26ae7cbcaf88beb5e40
Lucas Gruber <gruber.lucas05@gmail.com>
2026-05-23 16:38:19 +0200

    fix: fixed ship creation

--- app/utils/get-short-company-name.ts
+++ app/utils/get-short-company-name.ts
@@ -1,7 +1,7 @@
 import type { Company } from '~~/types';
 
-export function getCompanyCode (company: Company): string {
-  if (!company.name) return ''
+export function getCompanyCode (company: Company | null | undefined): string {
+  if (!company?.name) return ''
 
   if (company.name?.split(' ')?.length > 2 && company.code) return company.code
   else return company.name
